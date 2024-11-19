bb4d8c2d83d16cecf54d890c4c20bd33718340f9
Pogopeck <90612000+Pogopeck@users.noreply.github.com>
2024-11-19 18:05:56 +0530

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,32 @@
+# utils/logger.py
+import logging
+import os
+from datetime import datetime
+
+class TestLogger:
+    def __init__(self):
+        self.logger = logging.getLogger('PlaywrightTests')
+        self.logger.setLevel(logging.INFO)
+        
+        # Create logs directory if it doesn't exist
+        os.makedirs('logs', exist_ok=True)
+        
+        # File handler
+        file_handler = logging.FileHandler(
+            f'logs/test_run_{datetime.now().strftime("%Y%m%d_%H%M%S")}.log'
+        )
+        file_handler.setLevel(logging.INFO)
+        
+        # Console handler
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        
+        # Formatter
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        )
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
+        
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(console_handler)
